5491c6653eb973a660344abc933b858d236b75c8
lakshmi <lakshmireddy1625@gmail.com>
2021-10-27 12:27:55 +0530

    added hello world to local

---
+++ src/main/java/com/example/demo/hello/Hello.java
@@ -0,0 +1,14 @@
+package com.example.demo.hello;
+
+public class Hello {
+
+	public void m1() {
+		System.out.println("hello world welcome");
+	}
+	public static void main(String[] args) {
+		System.out.println("main method hello world");
+		Hello h=new Hello();
+		h.m1();
+	}
+	
+}
